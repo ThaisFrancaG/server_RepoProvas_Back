776c9de88f9d1ed0bc7d0ac0b77ed01e58de89f8
Thais <tfrancag@gmail.com>
2022-04-21 19:18:40 -0300

    feat. Added archiquecture and libs

---
+++ database.ts
@@ -0,0 +1,9 @@
+import dotenv from "dotenv";
+import pg from "pg";
+import pkg from "@prisma/client";
+
+dotenv.config();
+const { PrismaClient } = pkg;
+export const prisma = new PrismaClient();
+
+//o prisma é que eu uso no lugar de connection. E ai, no caso, pela forma como o prisma se organiza, ele já pega o database url direto pelo prisma.schema
