e670d46b74174fea4d2f4550c03d199851b426db
Thais <tfrancag@gmail.com>
2022-04-23 10:48:05 -0300

    feat. Fixed some details for better interation to front-end

--- src/middlewares/validateSchemas.ts
+++ src/middlewares/validateSchemas.ts
@@ -1,7 +1,6 @@
 import { NextFunction, Request, Response } from "express";
 
 export default function validateSchema(schema) {
-  console.log("chegou na validação de schema");
   return (req: Request, res: Response, next: NextFunction) => {
     const validation = schema.validate(req.body);
     if (validation.error) {
