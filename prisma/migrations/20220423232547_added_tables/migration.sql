695a5425556e265eabf94daa8ab38140292db8ec
Thais <tfrancag@gmail.com>
2022-04-23 20:27:14 -0300

    feat. Added table models to prisma.schema

---
+++ prisma/migrations/20220423232547_added_tables/migration.sql
@@ -0,0 +1,79 @@
+-- CreateTable
+CREATE TABLE "categories" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+
+    CONSTRAINT "categories_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "terms" (
+    "id" SERIAL NOT NULL,
+    "number" TEXT NOT NULL,
+
+    CONSTRAINT "terms_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "disciplines" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+    "termId" INTEGER NOT NULL,
+
+    CONSTRAINT "disciplines_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "teacher" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+
+    CONSTRAINT "teacher_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "teachersDisciplines" (
+    "id" SERIAL NOT NULL,
+    "teacherId" INTEGER NOT NULL,
+    "disciplineId" INTEGER NOT NULL,
+
+    CONSTRAINT "teachersDisciplines_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Tests" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+    "pdfUrl" TEXT NOT NULL,
+    "categoryId" INTEGER NOT NULL,
+    "teacherDisciplineId" INTEGER NOT NULL,
+
+    CONSTRAINT "Tests_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "categories_name_key" ON "categories"("name");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "terms_number_key" ON "terms"("number");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "disciplines_name_key" ON "disciplines"("name");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "teacher_name_key" ON "teacher"("name");
+
+-- AddForeignKey
+ALTER TABLE "disciplines" ADD CONSTRAINT "disciplines_termId_fkey" FOREIGN KEY ("termId") REFERENCES "terms"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "teachersDisciplines" ADD CONSTRAINT "teachersDisciplines_disciplineId_fkey" FOREIGN KEY ("disciplineId") REFERENCES "disciplines"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "teachersDisciplines" ADD CONSTRAINT "teachersDisciplines_teacherId_fkey" FOREIGN KEY ("teacherId") REFERENCES "teacher"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Tests" ADD CONSTRAINT "Tests_categoryId_fkey" FOREIGN KEY ("categoryId") REFERENCES "categories"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Tests" ADD CONSTRAINT "Tests_teacherDisciplineId_fkey" FOREIGN KEY ("teacherDisciplineId") REFERENCES "teachersDisciplines"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
