74e28d365e7aa0e929593452508f4272dcf4d84b
Thais <tfrancag@gmail.com>
2022-05-01 20:15:49 -0300

    reafctor. Change some tests messages to make it more semantic

--- tests/index.test.ts
+++ tests/index.test.ts
@@ -2,6 +2,20 @@ import app from "../src/app";
 import { prisma } from "../database.js";
 import supertest from "supertest";
 
+describe("GET/teachers", () => {
+  beforeEach(async () => {
+    await prisma.$executeRaw`TRUNCATE TABLE sessions CASCADE;`;
+    await prisma.$executeRaw`TRUNCATE TABLE users CASCADE;`;
+  });
+  afterAll(async () => {
+    await prisma.$disconnect();
+  });
+  it("Checking up seeding", async () => {
+    const teacherList = await supertest(app).get("/teachers");
+    expect(teacherList.status).toBe(200);
+  });
+});
+
 describe("POST /sign-up", () => {
   beforeEach(async () => {
     await prisma.$executeRaw`TRUNCATE TABLE sessions CASCADE;`;
@@ -11,7 +25,7 @@ describe("POST /sign-up", () => {
     await prisma.$disconnect();
   });
 
-  it("dados os dados de usuário para cadastro, e supondo que esses dados estejam corretos, deve retornar status 201", async () => {
+  it("Send complete data for sign-up, and expects that a new user is created and that a duplicated is supressed with an error return", async () => {
     const bodyOk = {
       email: "thaisteste@gmail.com",
       password: "123456",
@@ -24,31 +38,7 @@ describe("POST /sign-up", () => {
     expect(repeatedEmail.status).toEqual(409);
   });
 
-  it("dados os dados de usuário para cadastro, e supondo que esses dados estejam corretos, deve retornar status 201", async () => {
-    const bodyOk = {
-      email: "thaisteste@gmail.com",
-      password: "123456",
-      passwordCheck: "123456",
-    };
-    const bodyWrongPassword = {
-      email: "thaisteste@gmail.com",
-      password: "123456",
-      passwordCheck: "123455",
-    };
-    const bodyFormat = {};
-
-    const firstLogin = await supertest(app).post("/sign-up").send(bodyOk);
-    const repeatedEmail = await supertest(app).post("/sign-up").send(bodyOk);
-    const wrongPassword = await supertest(app)
-      .post("/sign-up")
-      .send(bodyWrongPassword);
-    const wrongFormat = await supertest(app).post("/sign-up").send(bodyFormat);
-
-    expect(firstLogin.status).toEqual(201);
-    expect(repeatedEmail.status).toEqual(409);
-  });
-
-  it("senha incorreta", async () => {
+  it("Send complete data for sign-up, however with a wrong password check", async () => {
     const bodyWrongPassword = {
       email: "thaisteste@gmail.com",
       password: "123456",
@@ -61,27 +51,9 @@ describe("POST /sign-up", () => {
     expect(wrongPassword.status).toEqual(409);
   });
 
-  it("body vazio", async () => {
-    const bodyVazio = {};
-
-    const wrongPassword = await supertest(app).post("/sign-up").send(bodyVazio);
-    expect(wrongPassword.status).toEqual(422);
+  it("Send data incorrectly, in order to be get by the Joi validation", async () => {
+    const body = {};
+    const invalidFormat = await supertest(app).post("/sign-up").send(body);
+    expect(invalidFormat.status).toEqual(422);
   });
 });
-
-describe("GET/teachers", () => {
-  beforeEach(async () => {
-    await prisma.$executeRaw`TRUNCATE TABLE sessions CASCADE;`;
-    await prisma.$executeRaw`TRUNCATE TABLE users CASCADE;`;
-  });
-  afterAll(async () => {
-    await prisma.$disconnect();
-  });
-
-  it("Conferir se o seeding foi direitinho", async () => {
-    const teacherList = await supertest(app).get("/teachers");
-    console.log(teacherList.body);
-    expect(teacherList.status).toBe(200);
-  });
-});
-// describe("POST /sign-in");
